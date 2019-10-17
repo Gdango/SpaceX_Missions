bcd565fcbc8a8ade1c6501503f36dca264494ede
Van Huynh <vthhuynh@ucdavis.edu>
2019-10-16 21:08:19 -0700

    first commit

--- SpaceX.sql
+++ SpaceX.sql
@@ -1,28 +1,41 @@
 Use SpaceX_Missions
 
 -- calculate failure percentage
-select
-"Failure Reason", count(*) / (select count(*) * 1.0 from SpaceX_Missions.dbo.SpaceX_Mission_Data$)
+/*select
+"Failure Reason", count(*)*100 / (select count(*) * 1.0 from SpaceX_Missions.dbo.SpaceX_Mission_Data$)
 from SpaceX_Missions.dbo.SpaceX_Mission_Data$
 where  "Mission Outcome" = 'Failure'
-group by "failure Reason";
+group by "failure Reason";*/
 
-select "Failure Reason", count("Mission Outcome")
+/*select "Failure Reason", count("Mission Outcome")
 from SpaceX_Mission_Data$ 
 where "Mission Outcome" = 'Failure'
-group by "Failure Reason";
+group by "Failure Reason";*/
 
 -- Result: Most failure was due to Collision During Launch
 -- Notes: Mission Outcome is Failure, there's not Landing
 
 
--- What type of Vehicle would lead to mission success?
-select "Vehicle Type", count("Mission Outcome")
+-- What type of Vehicle would lead to failure?
+select "Vehicle Type", count("Vehicle Type") as "Failure Quantity"
 from SpaceX_Mission_Data$
-where "Mission Outcome" = 'Success'
+where "Mission Outcome" = 'Failure'
 group by "Vehicle Type";
 
-select "Mission Outcome", count("Mission Outcome")
+select t1."Vehicle Type", t1."Failure Quantity", t2."Success Quantity"
+from
+((select "Vehicle Type", count("Vehicle Type") as "Failure Quantity"
+from SpaceX_Mission_Data$
+where "Mission Outcome" = 'Failure'
+group by "Vehicle Type as t1) and 
+(select "Vehicle Type", count("Vehicle Type") as "Success Quantity"
+from SpaceX_Mission_Data$
+where "Mission Outcome" = 'Success' as t2))
+join t1."Vehicle Type" = t2."Vehicle Type"
+
+
+
+select "Mission Outcome", count("Mission Outcome") as Quantity
 from SpaceX_Mission_Data$
 group by "Mission Outcome";
 
